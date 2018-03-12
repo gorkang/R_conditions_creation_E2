8edbc6d0a55319bff208961d25adc370ce276512
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-03-12 15:18:25 -0300

    convert script to func to iterate through list of items

--- item_export.R
+++ item_export.R
@@ -1,9 +1,3 @@
-# TODO: wrap items with qualtrics-format related codes using a placeholder within a prefabricated qualtrics-item template?
-# e.g.:
-# [QUALTRICS CODE]
-# placeholder_to_be_replaced_with_item
-# [QUALTRICS CODE]
-
 # TODO: create a qualtrics-compatible response type text
 # the only response type that is customized (to disease context) is the sequential guided question.
 # all the others should be generics.
@@ -61,8 +55,13 @@
 
 # Item formating ----------------------------------------------------------
 
+
+
+items2qualtrics <- function(list_of_items) {
+    
 # item to format to qualtrics
-item <- problems_numbered_ordered_responses[[1]][[1]]
+# item <- problems_numbered_ordered_responses[[1]][[1]]
+item <- list_of_items
 
 # 00. get item name (this should be the actual file-name) ####
 item_name <- gsub("\\*\\*(.*)\\*\\*.*", "\\1", item)
@@ -80,45 +79,6 @@ item_nameless <- gsub("\\*\\*.*\\*\\*\n\n(.*)", "\\1", item)
 # 02. item without response type
 item_responseless <- gsub("(.*)\n\n\n\n\n.*", "\\1", item_nameless)
 
-# TODO: define html-format (font, font-size, breaklines(?), html-links to imgs if necessary)
-
-# +++++++++++++++++++++++++++++++++++++++++
-# HTML codes ++++++++++++++++++++++++++++++
-hmtl_linebreak <- "<br>"
-hmtl_italic <- "<i>ITALIZE_THIS</i>"
-
-# parameters (size in pixels) +++++++++++++
-q_choice_size <- 16
-q_question_size <- 22
-
-# font size templates +++++++++++++++++++++
-html_question_font_size <- 
-  paste0('<span style="font-size:', q_question_size, 'px;">QUESTION_TEXT_TO_FORMAT</span>')
-
-html_choices_font_size <- 
-  paste0('<span style="font-size:', q_choice_size, 'px;">CHOICES_TEXT_TO_FORMAT</span>')
-
-# +++++++++++++++++++++++++++++++++++++++++
-# +++++++++++++++++++++++++++++++++++++++++
-
-# +++++++++++++++++++++++++++++++++++++++++
-# Qualtrics codes +++++++++++++++++++++++++
-qualtrics_advanced_format <- "[[AdvancedFormat]]"
-qualtrics_block_start <- "[[Block:block_name]]"
-qualtrics_pagebreak <- "[[PageBreak]]"
-
-qualtrics_question_text <- "[[Question:Text]]"
-qualtrics_question_singlechoice_vertical <- "[[Question:MC:SingleAnswer:Vertical]]"
-qualtrics_question_singlechoice_horizontal <- "[[Question:MC:SingleAnswer:Horizontal]]"
-# q_question_dropdown <- "[[Question:MC:DropDown]]"
-qualtrics_question_textentry <- "[[Question:TE]]"
-
-qualtrics_question_choices <- "[[Choices]]"
-
-# +++++++++++++++++++++++++++++++++++++++++
-# +++++++++++++++++++++++++++++++++++++++++
-
-
 # 03. Item (without response type)
 html_item_responseless <- gsub("QUESTION_TEXT_TO_FORMAT", item_responseless, html_question_font_size)
 html_item_responseless_breaks <- gsub("\n", hmtl_linebreak, html_item_responseless)
@@ -129,20 +89,38 @@ html_item_responseless_breaks <- gsub("\n", hmtl_linebreak, html_item_responsele
 hmtl_response_type <- readChar(con = "materials/qualtrics/input/qual_question_gb.txt", nchars = file.info("materials/qualtrics/input/qual_question_gb.txt")$size)
 
 # 05. Combine item elements
-item_to_export <- paste(
-  qualtrics_advanced_format, "\n",
-  qualtrics_question_singlechoice_horizontal, "\n",
-  html_item_responseless_breaks, "\n",
-  qualtrics_question_choices, "\n",
-  hmtl_response_type
+
+if (grepl("_gi", item_name)) {
   
-  , sep = "")
+  item_to_export <- paste(
+    qualtrics_advanced_format, "\n",
+    qualtrics_question_singlechoice_horizontal, "\n",
+    html_item_responseless_breaks, "\n",
+    qualtrics_question_choices, "\n",
+    hmtl_response_type
+    
+    , sep = "")
+  
+  
+} else if (!grepl("_gi", item_name)) {
+  
+  item_to_export <- paste(
+    qualtrics_advanced_format, "\n",
+    qualtrics_question_text, "\n",
+    html_item_responseless_breaks
+    
+    , sep = "")
+  
+}
 
 # 06. Write item to txt file
-item_name
+
 cat(item_to_export, file = paste0("materials/qualtrics/output/", item_name, ".txt"))
 
+}
 
+    
+    lapply(problems_numbered_ordered_responses, function(x) {lapply(x, items2qualtrics)})
 
 
 

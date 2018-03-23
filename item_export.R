8c57b7db6bb37c333f96477d9b6500dd7ae86311
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-03-23 15:09:07 -0300

    store html and qualtric codes in a list each

--- item_export.R
+++ item_export.R
@@ -19,41 +19,42 @@
 # Format parameters -------------------------------------------------------
 
 # HTML codes ******************************
-    # font
-    hmtl_linebreak <- "<br>"
-    hmtl_italic <- "<i>ITALIZE_THIS</i>"
-    
-    # font size
-    q_choice_size <- 16
-    q_question_size <- 22
-    
-    # font size templates
-    html_question_font_size <- 
-      paste0('<span style="font-size:', q_question_size, 'px;">QUESTION_TEXT_TO_FORMAT</span>')
-    
-    html_choices_font_size <- 
-      paste0('<span style="font-size:', q_choice_size, 'px;">CHOICES_TEXT_TO_FORMAT</span>')
+html_codes <- list(
+  # font
+  linebreak = "<br>",
+  italic = "<i>ITALIZE_THIS</i>",
+  # font size
+  choice_size = 16,
+  question_size = 22,
+  # font size templates
+  question_font_size =
+    paste0('<span style="font-size:', q_question_size, 'px;">QUESTION_TEXT_TO_FORMAT</span>'),
+  choices_font_size =
+    paste0('<span style="font-size:', q_choice_size, 'px;">CHOICES_TEXT_TO_FORMAT</span>')
+)    
 
 # Qualtrics codes *************************
-    # General
-    qualtrics_advanced_format <- "[[AdvancedFormat]]"
-    qualtrics_block_start <- "[[Block:block_name]]"
-    qualtrics_pagebreak <- "[[PageBreak]]"
-    # Questions
-    qualtrics_question_text <- "[[Question:Text]]"
-    qualtrics_question_singlechoice_vertical <- "[[Question:MC:SingleAnswer:Vertical]]"
-    qualtrics_question_singlechoice_horizontal <- "[[Question:MC:SingleAnswer:Horizontal]]"
-    # q_question_dropdown <- "[[Question:MC:DropDown]]"
-    qualtrics_question_textentry <- "[[Question:TE]]"
-    # Answers
-    qualtrics_question_choices <- "[[Choices]]"
+qualtric_codes <- list(
+  # General
+  advanced_format = "[[AdvancedFormat]]",
+  block_start = "[[Block:block_name]]",
+  pagebreak = "[[PageBreak]]",
+  # Questions
+  question_text = "[[Question:Text]]",
+  question_singlechoice_vertical = "[[Question:MC:SingleAnswer:Vertical]]",
+  question_singlechoice_horizontal = "[[Question:MC:SingleAnswer:Horizontal]]",
+  question_dropdown = "[[Question:MC:DropDown]]",
+  question_textentry = "[[Question:TE]]",
+  # Answers
+  question_choices = "[[Choices]]"
+)
 
 # Item formating ----------------------------------------------------------
-  
-    # function to convert txt files to qualtrics txt format
-    source("functions/items2qualtrics.R")
 
-    # convert txt files to qualtric text format
-    invisible(lapply(problems_numbered_ordered_responses, function(x) {lapply(x, items2qualtrics)}))
+# function to convert txt files to qualtrics txt format
+source("functions/items2qualtrics.R")
+
+# convert txt files to qualtric text format
+invisible(lapply(problems_numbered_ordered_responses, function(x) {lapply(x, items2qualtrics)}))
 
 

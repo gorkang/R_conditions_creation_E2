adf57b7551fb70e868a272b96f24cd6bd305cefb
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-03-27 17:28:14 -0300

    improve prevalence detection using regex

--- dev_export_follow_up.R
+++ dev_export_follow_up.R
@@ -1,6 +1,6 @@
 problems_numbered_ordered_responses_flat <- unlist(problems_numbered_ordered_responses, recursive = TRUE)
 
-item_test <- problems_numbered_ordered_responses_flat[[1]]
+item_test <- problems_numbered_ordered_responses_flat[[8]]
 
 item_test %>% cat
 
@@ -8,11 +8,17 @@ item_test
 
 # very horrible regex to grab the prevalence of each problem. the regex is anchored to breaklines and "Suppose" and "Imagine"
 regex_first_line <- ".*([0-9]{2}.*[0-9]{4}.*at the time of the screening).*"
+regex_first_line_a <-   ".*\\n\\n.*\\n\\n.*\\n\\n\\n(.*)\\n\\n.*\\n\\n.*\\n\\n\\n.*\\n\\n\\n\\n\\n.*\\n"
+regex_first_line_a <-   ".*\\n\\n.*\\n\\n.*\\n\\n\\n(.*)\\n\\n.*\\n\\n.*\\n\\n\\n.*\\n.*"
+
+# entre la pregunta y el response type dejar sólo 2 saltos de linea.
 
 gsub(regex_first_line, "\\1", item_test)
+gsub(regex_first_line_a, "\\1", item_test)
 
 
-lapply(problems_numbered_ordered_responses_flat, function(x) {gsub(regex_first_line, "\\1", x)})
+
+lapply(problems_numbered_ordered_responses_flat, function(x) {gsub(regex_first_line_a, "\\1", x)})
 
 
 

ec4ee47b8043fe67153ada6b92573304c724b365
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-03-28 16:19:18 -0300

    replace lapply with map calls

--- item_export.R
+++ item_export.R
@@ -60,20 +60,21 @@ paired_items_dir <- "materials/qualtrics/output/paired_items/"
 source("functions/items2qualtrics.R")
 
 # does it worth it to add this to the end of textual item preparation?
-problems_numbered_ordered_responses_flat <- as.list(unlist(problems_numbered_ordered_responses, recursive = TRUE))
+# problems_numbered_ordered_responses_flat <- as.list(unlist(problems_numbered_ordered_responses, recursive = TRUE))
 
-invisible(lapply(problems_numbered_ordered_responses_flat, function(x) {items2qualtrics(list_of_items = x, inputdir = response_types_dir, outputdir = separated_item_dir)}))
+problems_numbered_ordered_responses %>% 
+  map(~items2qualtrics(list_of_items = .x, responsesdir = response_types_dir, outputdir = separated_item_dir, removePlaceholders = TRUE)) %>% 
+  invisible()
 
 # pair items: different context, same presentation format, different ppv prob, same response type
 # function to pair items
 source("functions/pair_items.R")
+
 # items_txt <- dir(output_dir, pattern = ".txt")
 txt_files <- dir(separated_item_dir, pattern = ".txt")
 twins <- character(length(txt_files)/2)
 
-# debug(pair_items)
-invisible(lapply(txt_files, function(x) {pair_items(txt_files = x, separated_item_dir = separated_item_dir, twins = twins, outputdir = paired_items_dir)}))
-
+txt_files %>% map(~pair_items(txt_files = .x, separated_item_dir = separated_item_dir, twins = twins, outputdir = paired_items_dir)) %>% invisible()
 
 # Follow-up format and exporting ------------------------------------------
 

42b4fb0a0dfa2b37bb5b89f3443091f4ec5693a5
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-03-28 16:21:21 -0300

    adapt to use placeholders. fix call to qualtrics and html codes

--- functions/items2qualtrics.R
+++ functions/items2qualtrics.R
@@ -1,14 +1,17 @@
-items2qualtrics <- function(list_of_items) {
+items2qualtrics <- function(list_of_items, responsesdir, outputdir, removePlaceholders) {
   
   # item to format to qualtrics
   # item <- problems_numbered_ordered_responses[[1]][[1]]
-  item <- list_of_items
+  item <- 
+    list_of_items
   
   # 00. get item name (this should be the actual file-name) ####
-  item_name <- gsub("\\*\\*(.*)\\*\\*.*", "\\1", item)
+  item_name <- 
+    gsub("\\*\\*(.*)\\*\\*.*", "\\1", item)
   
   # 01. get item w/o name
-  item_nameless <- gsub("\\*\\*.*\\*\\*\n\n(.*)", "\\1", item)
+  item_nameless <- 
+    gsub("\\*\\*.*\\*\\*\n\n(.*)", "\\1", item)
   
   # separate problem context, actual item, and question from response type. ####
   # if all items end with five breaklines (\n) it can be use to chop-off the response type.
@@ -18,48 +21,60 @@ items2qualtrics <- function(list_of_items) {
   #                            function(x) {if (grep("\n\n\n\n\n", lapply(x, function(y) {y})) != 1) {print("NOOOOOOOO")}})})
   
   # 02. item without response type
-  item_responseless <- gsub("(.*)\n\n\n\n\n.*", "\\1", item_nameless)
+  item_responseless <- 
+    gsub("(.*)\\n\\[response_start\\].*", "\\1", item_nameless)
   
   # remove extra linebreaks
   # regex to select everything but extra breaklines
-  regex_pattern_breaklines <- "(^.*\\n\\n.*\\n\\n.*\\n)\\n(.*\\n)\\n(.*\\n\\n)\\n(.*$)"
+  # regex_pattern_breaklines <- "(^.*\\n\\n.*\\n\\n.*\\n)\\n(.*\\n)\\n(.*\\n\\n)\\n(.*$)"
   # remove extra breaklines
-  item_responseless <- gsub(regex_pattern_breaklines, "\\1\\2\\3\\4", item_responseless)
+  # item_responseless <- gsub(regex_pattern_breaklines, "\\1\\2\\3\\4", item_responseless)
   
-  # 03. Item (without response type)
-  html_item_responseless <- gsub("QUESTION_TEXT_TO_FORMAT", item_responseless, html_question_font_size)
-  html_item_responseless_breaks <- gsub("\n", hmtl_linebreak, html_item_responseless)
+  # 03. html format (item responseless)
+  html_item <- 
+    gsub("\n", html_codes$linebreak,
+         gsub("QUESTION_TEXT_TO_FORMAT", item_responseless, html_codes$question_font_size)
+    )
+  
   
   # 04. response type
   # Global intuitive response type
-  hmtl_response_type <- readChar(con = "materials/qualtrics/input/qual_question_gb.txt", nchars = file.info("materials/qualtrics/input/qual_question_gb.txt")$size)
+  hmtl_response_type <- 
+    readChar(con = responsesdir, nchars = file.info(responsesdir)$size)
   
   # 05. Combine item elements
   
   if (grepl("_gi", item_name)) {
     
-    item_to_export <- paste(
-      qualtrics_advanced_format, "\n",
-      qualtrics_question_singlechoice_horizontal, "\n",
-      html_item_responseless_breaks, "\n",
-      qualtrics_question_choices, "\n",
-      hmtl_response_type
-      
-      , sep = "")
+    item_to_export <- 
+      paste(
+        qualtrics_codes$advanced_format, "\n",
+        qualtrics_codes$question_singlechoice_horizontal, "\n",
+        html_item, "\n",
+        qualtrics_codes$question_choices, "\n",
+        hmtl_response_type
+        
+        , sep = "")
     
     
   } else if (!grepl("_gi", item_name)) {
     
     item_to_export <- paste(
-      qualtrics_advanced_format, "\n",
-      qualtrics_question_text, "\n",
-      html_item_responseless_breaks
+      qualtrics_codes$advanced_format, "\n",
+      qualtrics_codes$question_text, "\n",
+      html_item
       
       , sep = "")
     
   }
   
   # 06. Write item to txt file
-  cat(item_to_export, file = paste0("materials/qualtrics/output/", item_name, ".txt"))
+  dir.create(outputdir, showWarnings = FALSE, recursive = TRUE)
+  if (removePlaceholders == TRUE) {
+    cat(remove_placeholders(item_to_export), file = paste0(outputdir, item_name, ".txt"))
+  } else if (removePlaceholders == FALSE) {
+    cat(item_to_export, file = paste0(outputdir, item_name, ".txt"))
+    
+  }
   
 }

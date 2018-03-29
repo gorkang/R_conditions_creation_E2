95733314554b7bbe594edfae2e0d298b5c705843
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-03-29 15:52:50 -0300

    uptade todo, remove temp lines

--- dev_export_follow_up.R
+++ dev_export_follow_up.R
@@ -3,23 +3,19 @@
 # There are 2 follow-up risk (1%, 10%)
 # Therefore, there are 32 possible follow-up items (e.g. ca_nfab_low_high; ppvLow_riskHigh)
 
-# TODO: iterate through 16 prevalences and paste them into the follow-up template (1%, 10%). Use the same string where the prevalence is to get the name
-
+# TODO: add qualtric/html format to export
 
 item_test <- problems_numbered_ordered_responses[[10]]
 
 item_test %>% cat
 
-uniqchars <- function(x) unique(strsplit(x, "")[[1]]) 
+uniqchars <- 
+  function(x) unique(strsplit(x, "")[[1]]) 
 
 response_type_regex <-
   dir("materials/Response_type/", pattern = ".txt") %>% 
   gsub("\\.txt", "", .) %>% paste(., collapse = "") %>% 
   uniqchars() %>% paste(., collapse = "")
-
-
-# gsub(paste0("\\*\\*(.*)_[", response_type_regex, "]{2}\\*\\*.*"), "\\1", problems_numbered_ordered_responses)
-
 
 # This snippet get only names
 # unique_prevalences_names <-

c3ef6480a6c66a406fa486f53ff48c0616b53b17
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-03-29 15:43:18 -0300

    add followup placeholder removal

--- functions/funcs_item_displaying.R
+++ functions/funcs_item_displaying.R
@@ -1,14 +1,24 @@
 # This function takes strings and remove all strings defined on placeholders vector, and
 # outputs the new string
 
-remove_placeholders <- function(items) {
+remove_placeholders <- function(items, item_followup) {
   
   # all placeholders within items, contexts, questions and responses must be in this vector
-  placeholders <- 
-    c("first_piece","second_piece","third_piece","item_end",
-      "context_start","context_mid","context_end",
-      "question_start","question_end",
-      "response_start","response_mid","response_end")  
+  if (item_followup == "item") {
+    
+    placeholders <- 
+      c("first_piece","second_piece","third_piece","item_end",
+        "context_start","context_mid","context_end",
+        "question_start","question_end",
+        "response_start","response_mid","response_end")
+    
+  } else if (item_followup == "followup") {
+    
+    placeholders <- 
+      c("followUp_start", "followUp_first_piece", "followUp_second_piece", "followUp_third_piece", 
+        "questions_start", "second_question", "third_question", "fourth_question", "fifth_question", "followUp_end")
+    
+  }
   
   # create regex to detect all placeholders
   placeholders_regex <-
@@ -18,7 +28,6 @@ remove_placeholders <- function(items) {
   gsub(placeholders_regex, "", items)
   
 }
-
 
 # This function takes a string indicating a condition [problemContext_presentationFormat_ppvProb].
 # Each info must be separated by a "_".

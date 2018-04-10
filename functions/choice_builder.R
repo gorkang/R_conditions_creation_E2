425f7871afc8917f10bfb791190ff7cb2795c80c
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-04-10 12:58:07 -0300

    final version of choice builder

--- functions/choice_builder.R
+++ functions/choice_builder.R
@@ -1,96 +1,71 @@
-# This has to be put on the choices part within each question building
-
-
-# This object are defined once for all future calls. 
-# Possible number of choices 
-ordinal_numbers <- 
-  c("second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth")
-
-# This two are set for the current call
-# Number of choices
-choices_number <- 
-  (sum(str_detect(pattern = paste0(ordinal_numbers, "_choice"), string = response_text)) + 1)
-# Actual choices
-choices <- 
-  c("first", ordinal_numbers[seq(choices_number-1)])
-
+# Debug ++++++++++++++++++++++++++
+# response_string <- response_text
+# x <- choices[2]
+# extract_choice_text(x)
 
 choice_builder <- 
-  function(choices_ordinal, response_string) {
+  function(response_string) {
     
-    choices_ordinal <- 
-      choices
-    # response_string <- response_text
-    
-    xxxx <- choices_ordinal %>% 
-      map(~custom_xxx(.x)) %>% unlist
-    
-    x <- choices_ordinal[6]
-    
-    choices_ordinal %>%
-      map(~custom_xxx(.x))
-    
-    parent.env()
-    
-    custom_xxx <- 
+    # secondary function definition +++++++++++++++++++++++++++
+    # this is the function that do the actual work. the choice
+    # builder is just a wrap-up to iteratively call this func.
+    extract_choice_text <- 
       function (x) {
-      if (length(choices) == 1) {
-        gsub("CHOICES_TEXT_TO_FORMAT",
-             extract_between_placeholders("choiches_start", "second_choice", response_string),
-             html_codes$choices_font_size)
-      }  else if (length(choices) < 3) {
-        if (x == "first") {
+        if (length(choices) == 1) {
           gsub("CHOICES_TEXT_TO_FORMAT",
-               extract_between_placeholders("choiches_start", "second_choice", response_string),
+               extract_between_placeholders("choices_start", "choices_end", response_string),
                html_codes$choices_font_size)
-        } else if (x == "second") {
-          gsub("CHOICES_TEXT_TO_FORMAT",
-               extract_between_placeholders("second_choice", "choiches_end", response_string),
-               html_codes$choices_font_size)
-        }
-      } else if (length(choices) >= 3) {
-        if (x == "first") {
-          .GlobalEnv$counter <- 1
-          gsub("CHOICES_TEXT_TO_FORMAT",
-               extract_between_placeholders("choiches_start", "second_choice", response_string),
-               html_codes$choices_font_size)
-        } else if (!which(choices %in% x) == length(choices)) {
-          gsub("CHOICES_TEXT_TO_FORMAT",
-               extract_between_placeholders(paste0(x, "_choice"), paste0(choices[counter+2], "_choice"), response_string),
-               html_codes$choices_font_size)
-        } else if (which(choices %in% x) == length(choices)) {
-          gsub("CHOICES_TEXT_TO_FORMAT",
-               extract_between_placeholders(paste0(x,"_choice"), paste0("choices_end"), response_string),
-               html_codes$choices_font_size)
+        }  else if (length(choices) < 3) {
+          if (x == "first") {
+            gsub("CHOICES_TEXT_TO_FORMAT",
+                 extract_between_placeholders("choices_start", "second_choice", response_string),
+                 html_codes$choices_font_size)
+          } else if (x == "second") {
+            gsub("CHOICES_TEXT_TO_FORMAT",
+                 extract_between_placeholders("second_choice", "choices_end", response_string),
+                 html_codes$choices_font_size)
+          }
+        } else if (length(choices) >= 3) {
+          if (x == "first") {
+            gsub("CHOICES_TEXT_TO_FORMAT",
+                 extract_between_placeholders("choices_start", "second_choice", response_string),
+                 html_codes$choices_font_size)
+          } else if (!which(choices %in% x) == length(choices)) {
+            gsub("CHOICES_TEXT_TO_FORMAT",
+                 extract_between_placeholders(paste0(x, "_choice"), paste0(choices[which(choices %in% x) + 1], "_choice"), response_string),
+                 html_codes$choices_font_size)
+          } else if (which(choices %in% x) == length(choices)) {
+            gsub("CHOICES_TEXT_TO_FORMAT",
+                 extract_between_placeholders(paste0(x,"_choice"), paste0("choices_end"), response_string),
+                 html_codes$choices_font_size)
+          }
         }
       }
-    }
     
+    # arguments for every call ++++++++++++++++++++++++++++++++
+    # Possible number of choices 
+    ordinal_numbers <- 
+      c("second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth")
+    # For this particular call ++++++++++++++++++++++++++++++++
+    # Number of choices
+    choices_number <- 
+      (sum(str_detect(pattern = paste0(ordinal_numbers, "_choice"), string = response_string)) + 1)
+    # Actual choices
+    choices <-
+      if (choices_number == 1) {
+        c("first")
+      } else if (choices_number != 1) {
+        c("first", ordinal_numbers[seq(choices_number-1)])
+        
+      }
     
+    # Temp stuff ++++++++++++++++++++++++++++++++++++++++++++++
+    # Create a temp copy of choices to be able to check "choices" after manipualted the temp copy
+    choices_ordinal <- 
+      choices
     
-    # print_choices <- function() {
-    #   if (choices_ordinal == "first") {
-    #     print(gsub("CHOICES_TEXT_TO_FORMAT",
-    #                extract_between_placeholders("choiches_start", "second_choice", response_string),
-    #                html_codes$choices_font_size))
-    #     .GlobalEnv$next_next_choice <-
-    #       .GlobalEnv$choices[which(.GlobalEnv$choices %in% choices_ordinal) + 2]
-    #   
-    #     } else if (!which(.GlobalEnv$choices %in% choices_ordinal) == length(.GlobalEnv$choices)) {
-    #     print(gsub("CHOICES_TEXT_TO_FORMAT",
-    #                extract_between_placeholders(paste0(choices_ordinal,"_choice"), paste0(next_next_choice, "_choice"), response_string),
-    #                html_codes$choices_font_size))
-    #     .GlobalEnv$next_next_choice <-
-    #       .GlobalEnv$choices[which(.GlobalEnv$choices %in% choices_ordinal) + 2]
-    #   } else if (which(.GlobalEnv$choices %in% choices_ordinal) == length(.GlobalEnv$choices)) {
-    #     print(gsub("CHOICES_TEXT_TO_FORMAT",
-    #                extract_between_placeholders(paste0(choices_ordinal,"_choice"), paste0("choices_end"), response_string),
-    #                html_codes$choices_font_size))
-    #   }
-    #   
-    # }
-    # 
-    # choices_ordinal %>% map(~print_choices(.))
-    
+    # Iterative call to extract choice text
+    choices_ordinal %>%
+      map(~extract_choice_text(.x))
     
   }

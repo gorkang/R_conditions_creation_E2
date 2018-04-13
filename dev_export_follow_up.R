49b7bc2fe8bb72fbdfb00bce9994943ec6615322
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-04-13 15:18:36 -0300

    improve commenting

--- dev_export_follow_up.R
+++ dev_export_follow_up.R
@@ -5,7 +5,10 @@
 # There are 2 follow-up risk (1%, 10%)
 # Therefore, there are 32 possible follow-up items (e.g. ca_nfab_low_high; ppvLow_riskHigh)
 
-# Functions
+
+# Functions ----------------------
+# All the following functions are exclusively for dealing with follow-up qualtrics-html building and export
+
 source("scripts/html_qualtrics_codes.R")
 source("functions/extract_between_placeholders.R")
 source("functions/choice_builder.R")
@@ -14,26 +17,24 @@ source("functions/followup_question_builder.R")
 source("functions/load_puthtml_export.R")
 source("functions/export_qualtrics_followup_items.R")
 
-# ALL PATHS ****************************
+# ALL PATHS ----------------------
+
 path2fu_raw_questions              <- "materials/Question/Follow_up/input/questions_raw/"
 path2fu_w_prev                     <- "materials/Question/Follow_up/output/item_w_prevalence/"
 path2fu_qualtrics_items            <- "materials/qualtrics/input/followUp/items/"
 path2fu_qualtrics_questions        <- "materials/qualtrics/input/followUp/questions/"
 path2fu_qualtrics_complete_items   <- "materials/qualtrics/output/followUp/"
 
-# **************************************
 
-# ##################################
-# Follow-up questions building
-# ##################################
+# Follow-up questions building ----
+# iterates through files in file_path, identifies how choices the question has, build choices and question and export them (default) or paste them
 
 dir(path2fu_raw_questions, ".txt") %>% 
-  map(~followup_question_builder(path2fu_raw_questions, .x, path2fu_qualtrics_questions)) %>% 
+  map(~followup_question_builder(file_path = path2fu_raw_questions, file_name = .x, outputdir = path2fu_qualtrics_questions)) %>% 
   invisible()
 
-# ###################################################
-# Follow-up item building with unique prevalences
-# ###################################################
+# Follow-up item building with unique prevalences --------
+# Get unique response types to get all unique prevalences. These are used to create unique follow-up items.
 
 # function to get unique character within string?
 uniqchars <- 
@@ -61,27 +62,30 @@ unique_prevalences %>%
                      outputdir = "materials/Question/Follow_up/output/item_w_prevalence/", rmv_placeholders = TRUE) ) %>% 
   invisible()
 
-# ###################################################
-# Bind follow-up items with questions (customizing by problem context)
-# ###################################################
+# Bind follow-up items with questions (customizing by problem context) ------------------
 
+# put html tags around follow-up items (already with prevalences)
+# path to follow-up items with prevalence 
 files <- 
   dir(path2fu_w_prev, ".txt")
-
+# paste html tags
 files %>% 
   map(~load_puthtml_export(.x)) %>% 
   invisible()
 
-# Bind with questions (by problem context)
+# Bind items with questions (by problem context)
+# follow-up items (already with html tags and prevalences) files
 item_files <- 
   dir(path2fu_qualtrics_items, ".txt")
+# follow-up question files
 question_files <- 
   dir(path2fu_qualtrics_questions, ".txt")
-
+# vector with questions to paste to items
 questions <- question_files %>% 
   map(~readChar(con = paste0(path2fu_qualtrics_questions,.x), nchars = file.size(paste0(path2fu_qualtrics_questions,.x)))) %>% 
   unlist
 
+# paste and export all together 
 item_files %>% 
   map(~export_qualtrics_followup_items(.x)) %>% 
   invisible()

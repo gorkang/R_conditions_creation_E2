9260bada143fc3fa08715565311fc13f23851baf
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-04-25 16:06:41 -0300

    use context to dinamically build medical condition to fill with. Fix related risk percentage regex

--- functions/export_qualtrics_followup_items.R
+++ functions/export_qualtrics_followup_items.R
@@ -20,34 +20,32 @@ export_qualtrics_followup_items <- function(x) {
   # #########################################################################
   
   # x <- item_files[1]
+  # x <- item_files[40]
   x_context <- gsub(paste0(".*(", problem_contexts, ").*"), "\\1", x) # get context
   x_item <- readChar(con = paste0(path2fu_qualtrics_items,x), nchars = file.info(paste0(path2fu_qualtrics_items,x))$size) # read item
   x_item <- gsub("\\*\\*\\*.*\\*\\*\\*","", x_item) # remove name between ***name***
   
   dir.create(path2fu_qualtrics_complete_items, showWarnings = FALSE, recursive = TRUE)
   
-  if (x_context == "ca") {
-    
-    cat(qualtrics_codes$advanced_format, "\n",
-        qualtrics_codes$question_only_text, "\n",
-        gsub("\n", html_codes$linebreak, x_item), "\n",
-        gsub("risk_percentage", 
-             gsub(".*([0-9]{2}%).*", "\\1", x_item), 
-             gsub("medical_condition", "breast cancer", paste(questions, collapse = "\n"))),
-        sep = ""
-        , file = paste0(path2fu_qualtrics_complete_items, x)
-    )
-  } else if (x_context == "pr") {
-    
-    cat(qualtrics_codes$advanced_format, "\n",
-        qualtrics_codes$question_only_text, "\n",
-        gsub("\n", html_codes$linebreak, x_item), "\n",
-        gsub("risk_percentage", 
-             gsub(".*([0-9]{2}%).*", "\\1", x_item), 
-             gsub("medical_condition", "Trisomy 21", paste(questions, collapse = "\n"))),
-        sep = ""
-        , file = paste0(path2fu_qualtrics_complete_items, x)
-    )
-  }
+  # MANUAL MODIFICATION #####################
+  # csv with fillers for medical condition
+  fillers <- 
+    read_csv("materials/Response_type/sg_fillers/sg_fillers.csv", col_types = cols())
+  
+  current_context_index <- 
+    grep(x_context, fillers$item_cond)
+  
+  fillers[[current_context_index, "CONDITION"]]
+  # ##########################################
+  
+  cat(qualtrics_codes$advanced_format, "\n",
+      qualtrics_codes$question_only_text, "\n",
+      gsub("\n", html_codes$linebreak, x_item), "\n",
+      gsub("risk_percentage", 
+           gsub(".*has a ([0-9]+%).*", "\\1", x_item), 
+           gsub("medical_condition", fillers[[current_context_index, "CONDITION"]], paste(questions, collapse = "\n"))),
+      sep = ""
+      , file = paste0(path2fu_qualtrics_complete_items, x)
+  )
   
 }

9fa3197b9878994c1cadbb39529547d1b2cfd70f
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-05-15 18:48:22 -0400

    capture textual problems prevalences

--- scripts/bayes_item_export.R
+++ scripts/bayes_item_export.R
@@ -23,4 +23,41 @@ txt_files <- dir(separated_item_dir, pattern = ".txt")
 twins <- character(length(txt_files)/2)
 
 txt_files %>% 
-  walk(~pair_items(txt_files = .x, separated_item_dir = separated_item_dir, twins = twins, outputdir = paired_items_dir))
+  walk(~pair_items(txt_files = .x, separated_item_dir = separated_item_dir, twins = twins, outputdir = paired_items_dir))# Pair each item with its correspondent follow-up -------------------------------------------------------------------------
+# Pair each item with its correspondent follow-up -------------------------------------------------------------------------
+
+# If each item prevalence can be captured on the fly only two follow-up templates are needed. 
+# # If each follow-up have to be previously build, then 12 follow-up templates are needed.
+# Using placeholders the follow-up can be captured.
+
+# Textual problems prevalences #####
+
+# Custom func to capture prevalences
+capture_prevalence <- function(str) {
+  prev <-
+    str %>% 
+    gsub(".*\\[first_piece\\](.*)\\[second_piece\\].*", "\\1", .) %>% 
+    gsub("\\n", "", .)
+  name <-
+    str %>% 
+    gsub(".*\\*\\*(.*)\\*\\*.*", "\\1", .) %>% 
+    gsub("(.*_.*_.*)_.*", "\\1", .)
+  
+  c(name, prev) 
+}
+
+# iterate over problems to capture prevalences
+prevalences <- 
+  problems_numbered_ordered_responses %>% 
+  map(., capture_prevalence)
+# remove duplicates (each [presentation format x ppv] prevalence is duplicated 4 times (response types))
+prevalences <- 
+  prevalences[!duplicated(prevalences)]
+# convert list with names and prevalences to a nice table
+named_prevalences <- 
+  unlist(prevalences) %>% 
+  matrix(., nrow = 16, byrow = T) %>% 
+  as.tibble(.) %>% 
+  rename(name = V1, prevalence = V2)
+
+# Pictorial problems prevalences

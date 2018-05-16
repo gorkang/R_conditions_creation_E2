9c83960c71808152afe18735ba26eeebe9deb76c
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-05-16 15:04:31 -0400

    remove old comments. relocate temp pairing

--- scripts/bayes_item_export.R
+++ scripts/bayes_item_export.R
@@ -8,29 +8,7 @@ response_types_dir <- "materials/qualtrics/input/reponse_type/"
 paired_items_dir   <- "materials/qualtrics/output/paired_items/"
 
 
-# does it worth it to add this to the end of textual item preparation?
-# problems_numbered_ordered_responses_flat <- as.list(unlist(problems_numbered_ordered_responses, recursive = TRUE))
-
-problems_numbered_ordered_responses %>% 
-  walk(~items2qualtrics(list_of_items = .x, responsesdir = response_types_dir, outputdir = separated_item_dir, removePlaceholders = TRUE))
-
-# # pair items: different context, same presentation format, different ppv prob, same response type
-# # function to pair items
-# source("functions/pair_items.R")
-# 
-# # items_txt <- dir(output_dir, pattern = ".txt")
-# txt_files <- dir(separated_item_dir, pattern = ".txt")
-# twins <- character(length(txt_files)/2)
-# 
-# txt_files %>%
-#   walk(~pair_items(txt_files = .x, separated_item_dir = separated_item_dir, twins = twins, outputdir = paired_items_dir))
-
-
-# Pair each item with its correspondent follow-up -------------------------------------------------------------------------
-
-# If each item prevalence can be captured on the fly only two follow-up templates are needed. 
-# # If each follow-up have to be previously build, then 12 follow-up templates are needed.
-# Using placeholders the follow-up can be captured.
+# All possible prevalences ------------------------------------------------
 
 # Textual problems prevalences #####
 
@@ -150,3 +128,20 @@ pic_prevalences_str_filled <-
 all_prevalences <- 
   named_prevalences %>% 
   bind_rows(pic_prevalences_str_filled)
+
+# Convert items to qualtrics txt advanced format --------------------------
+problems_numbered_ordered_responses %>% 
+  walk(~items2qualtrics(list_of_items = .x, responsesdir = response_types_dir, outputdir = separated_item_dir, removePlaceholders = TRUE))
+
+# # pair items: different context, same presentation format, different ppv prob, same response type
+# # function to pair items
+# source("functions/pair_items.R")
+# 
+# # items_txt <- dir(output_dir, pattern = ".txt")
+# txt_files <- dir(separated_item_dir, pattern = ".txt")
+# twins <- character(length(txt_files)/2)
+# 
+# txt_files %>%
+#   walk(~pair_items(txt_files = .x, separated_item_dir = separated_item_dir, twins = twins, outputdir = paired_items_dir))
+
+

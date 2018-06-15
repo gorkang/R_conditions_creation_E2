ce102b5a2fe5acbda0064cd6a258607af69a6b75
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-06-15 13:07:45 -0400

    js codes to capture ppv generation. first version

---
+++ js_capture_ppv.R
@@ -0,0 +1,67 @@
+
+# 
+get_other_questions_ID_num <- function(other_questions) {
+  # Receives a number indicating how many questions (other than the first) have to be created.
+  # Outputs a chr vector
+  seq(other_questions) %>% 
+    map_chr(~paste0("var qid_0", .x+1, "_num = qid_0", .x, "_num + ", .x, ";")) %>% 
+    paste(., collapse = "\n")
+}
+# 
+get_other_questions_responses <- function(other_questions) {
+  # Receives a number indicating how many responses (other than the first) have to be captured
+  # Outputs a chr vector
+  seq(other_questions) %>% 
+    map_chr(~paste0(
+      "var responseTextField_0", 
+      .x+1, " = document.getElementById('QR~QID' + qid_0", 
+      .x+1, "_num);\nvar currentResponse_0", 
+      .x+1, " = responseTextField_0", .x+1, ".value;")) %>% 
+    paste(., collapse = "\n")
+}
+# 
+get_captured_resp_consolelog <- function(all_questions) {
+  # Receives a number indicating how many console logs have to be created to display captured responses (including the first).
+  # Outputs a chr vector
+  seq(all_questions) %>% 
+    map_chr(~paste0("/* console.log('Captured text entry response ", .x, " is: ' + currentResponse_0", .x, ") */")) %>% 
+    paste(., collapse = "\n")
+}
+
+page_submit <- 
+  "Qualtrics.SurveyEngine.addOnPageSubmit(function()\n{\n\nREPLACE_THIS\n\n});"
+commented <- 
+  "\n/* REPLACE_THIS */"
+get_id <- 
+  "var qid_01_str = this.questionId;"
+get_id_num <- 
+  "var qid_01_num = Number(qid_01_str.replace(/^\\\\D+/g, ''));" # extra / because it's a metacharacter
+consolelog <- # by default console logs are commented
+  "\n/* console.log(REPLACE_THIS) */"
+get_response <- 
+  "var responseTextField_01 = document.getElementById('QR~QID' + qid_01_num);\nvar currentResponse_01 = responseTextField_01.value;"
+ss_create_ppv_response <- 
+  "\n/* Create PPV response */\nvar ppv_response_01 = currentResponse_01 + ' out of ' + currentResponse_02;\n/* console.log('ppv response is: ' + ppv_response_01); */"
+assign_ppv_to_ED <- 
+  "\n/* If ED exists, assign value to it. If does not exists, create it with indicated value */\nQualtrics.SurveyEngine.setEmbeddedData('ppv_response_01', ppv_response_01)"
+
+# block
+other_questions <- 1
+
+paste(gsub("REPLACE_THIS", "Get current question ID (e.g. QID10)", commented),
+      get_id,
+      gsub("REPLACE_THIS", "Get questions IDs (e.g. 10, 11, 12, etc.)", commented),
+      get_id_num,
+      get_other_questions_ID_num(other_questions),
+      gsub("REPLACE_THIS", "'Question is: QR~' + qid_01_str", consolelog),
+      gsub("REPLACE_THIS", "Save current question input response", commented),
+      get_response,
+      get_other_questions_responses(other_questions),
+      get_captured_resp_consolelog(other_questions+1),
+      ss_create_ppv_response,
+      assign_ppv_to_ED,
+      sep = "\n") %>% 
+  gsub("\n", "\n   ", .) %>% 
+  gsub("REPLACE_THIS", ., page_submit) %>%
+  cat(., file = "materials/qualtrics/output (2)/plain_text/js_codes/ss_capture_ppv.txt")
+

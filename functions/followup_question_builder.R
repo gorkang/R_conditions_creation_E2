07233e3cb10731bd61546aeebd18a8c206c8032e
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-07-06 10:43:34 -0400

    fix question and id capture

--- functions/followup_question_builder.R
+++ functions/followup_question_builder.R
@@ -21,7 +21,7 @@ followup_question_builder <- function(file_path, file_name, outputdir, export = 
     # ACTUAL FUNCTION
     cat(
       # QUESTION TYPE ********************
-      gsub("(\\[\\[.*\\]\\]).*", "\\1", question_text),
+      gsub("(\\[\\[Question\\:.*\\]\\]).*", "\\1", question_text),
       
       # QUESTION ********************
       gsub("QUESTION_TEXT_TO_FORMAT", 
@@ -41,7 +41,10 @@ followup_question_builder <- function(file_path, file_name, outputdir, export = 
   } else if (export == FALSE) {
     paste(
       # QUESTION TYPE ********************
-      gsub("(\\[\\[.*\\]\\]).*", "\\1", question_text),
+      gsub(".*(\\[{2}Question.*?\\]{2}).*", "\\1", question_text),
+      
+      # QUESITON ID **********************
+      gsub(".*(\\[{2}ID\\:.*?\\]{2}).*", "\\1", question_text),
       
       # QUESTION ********************
       gsub("QUESTION_TEXT_TO_FORMAT", 

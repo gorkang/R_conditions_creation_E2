58debf3c3324b21e8bed25ddb28c1022a95ab010
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-02 16:44:00 -0400

    add big five inventory

---
+++ scripts/scale_big_five.R
@@ -0,0 +1,25 @@
+# Packages
+if (!require('pacman')) install.packages('pacman'); library('pacman')
+p_load(tidyverse, magrittr)
+
+# Template reading and handling
+apriori_scale <-
+  "materials/Scales/input/big_five.txt" %>% 
+  readChar(., file.size(.)) %>% # Read text file
+  str_split(., "\n__QSEP__\n") %>% # Separate questions
+  unlist() %>% as.list() %>% # Flatten questions list
+  str_replace_all(., "replaceID", paste0("big_five_", sprintf("%02d", seq(length(.))))) %>% # Add question IDs
+  gsub("Q_FONT_SIZE", question_size, .) %>% # Change Questions Font size
+  gsub("C_FONT_SIZE", choice_size, .) # Change Choices Font size
+
+# Output dir
+output_dir <- 
+  "materials/qualtrics/output/plain_text/scales/big_five" %T>% 
+  dir.create(., FALSE, TRUE)
+# Export
+apriori_scale %>% 
+  cat(qualtrics_codes$advanced_format, 
+      qualtrics_codes$block_start %>% gsub("block_name", "big_five", .), 
+      ., sep = "\n", 
+      file = file.path(output_dir, "big_five.txt"))
+

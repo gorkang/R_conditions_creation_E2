b0bf6ff2dcbfbafbfab151713efdaba8b9cae17b
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-02 15:04:23 -0400

    use trycatch when trying to log into qualtrics

--- functions/get2survey.R
+++ functions/get2survey.R
@@ -4,19 +4,32 @@ get2survey <- function(survey_link) {
   remDr$navigate(survey_link)
   Sys.sleep(1)
   
-  # User name
-  username <- remDr$findElement(using = "css selector", value = '#UserName') # username text-entry element
-  username$clearElement() # click element
-  username$sendKeysToElement(list("gorka.navarrete@uai.cl")) # send username to username field
+  tryCatch(expr = {
+    # TRY
+    
+    # User name
+    username <- remDr$findElement(using = "css selector", value = '#UserName') # username text-entry element
+    username$clearElement() # click element
+    username$sendKeysToElement(list("gorka.navarrete@uai.cl")) # send username to username field
+    
+    # Password
+    passwd <- remDr$findElement(using = "css selector", value = '#UserPassword') # password text-entry element
+    passwd$clearElement() # click element
+    passwd$sendKeysToElement(list(pass)) # send password (previously set on object "pass")
+    
+    # Sing in
+    webElem <- remDr$findElement(using = 'css selector', value = '#loginButton')
+    webElem$clickElement()
+    Sys.sleep(1)
+    
+  }, 
+  # ERROR
+  error = function(e) {
+    .GlobalEnv$n <- .GlobalEnv$n +1
+    
+    return("Already logged.")
+  })
   
-  # Password
-  passwd <- remDr$findElement(using = "css selector", value = '#UserPassword') # password text-entry element
-  passwd$clearElement() # click element
-  passwd$sendKeysToElement(list(pass)) # send password (previously set on object "pass")
   
-  # Sing in
-  webElem <- remDr$findElement(using = 'css selector', value = '#loginButton')
-  webElem$clickElement()
-  Sys.sleep(1)
   
 }

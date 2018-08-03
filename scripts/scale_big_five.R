4410a6854df738b9540a992c0bb186caff36041f
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-03 12:33:04 -0400

    build scale using identical wrapper with qualtrics tags for each item

--- scripts/scale_big_five.R
+++ scripts/scale_big_five.R
@@ -2,24 +2,45 @@
 if (!require('pacman')) install.packages('pacman'); library('pacman')
 p_load(tidyverse, magrittr)
 
-# Template reading and handling
-apriori_scale <-
+# names for item ID
+long_name <- "big_five_inventory"
+short_name <- "big5"
+
+# qualtrics tags template to wrapp around
+# instructions
+ins_wrapper <- '[[Question:Text]]\n[[ID:replaceID]]\n<span style="font-size:Q_FONT_SIZEpx;">ITEM</span>'
+# items
+item_wrapper <- '[[Question:MC:SingleAnswer:Horizontal]]\n[[ID:replaceID]]\n<span style="font-size:Q_FONT_SIZEpx;">ITEM</span>\n[[Choices]]\n<span style="font-size:C_FONT_SIZEpx;">Disagree Strongly</span>\n<span style="font-size:C_FONT_SIZEpx;">Disagree a little</span>\n<span style="font-size:C_FONT_SIZEpx;">Neither agree<br>nor disagree</span>\n<span style="font-size:C_FONT_SIZEpx;">Agree a little</span>\n<span style="font-size:C_FONT_SIZEpx;">Agree strongly</span>'
+# see what's going on.
+item_wrapper %>% cat()
+
+# read items
+big5_items <- 
   "materials/Scales/input/big_five.txt" %>% 
-  readChar(., file.size(.)) %>% # Read text file
-  str_split(., "\n__QSEP__\n") %>% # Separate questions
-  unlist() %>% as.list() %>% # Flatten questions list
-  str_replace_all(., "replaceID", paste0("big_five_", sprintf("%02d", seq(length(.))))) %>% # Add question IDs
-  gsub("Q_FONT_SIZE", question_size, .) %>% # Change Questions Font size
-  gsub("C_FONT_SIZE", choice_size, .) # Change Choices Font size
+  readChar(., file.size(.)) %>% 
+  gsub("\\n$", "", .) %>% 
+  str_split(., "\\n") %>% 
+  unlist()
+
+# Wrapping
+# instructions
+ins <- gsub("ITEM", big5_items[1], ins_wrapper)
+# items
+items <- str_replace_all(item_wrapper, "ITEM", big5_items[-1])
 
 # Output dir
 output_dir <- 
   "materials/qualtrics/output/plain_text/scales/big_five" %T>% 
   dir.create(., FALSE, TRUE)
-# Export
-apriori_scale %>% 
-  cat(qualtrics_codes$advanced_format, 
-      qualtrics_codes$block_start %>% gsub("block_name", "big_five", .), 
-      ., sep = "\n", 
-      file = file.path(output_dir, "big_five.txt"))
 
+# build and export scale
+c(ins, items) %>% 
+  gsub("Q_FONT_SIZE", question_size, .) %>% # Change Questions Font size
+  gsub("C_FONT_SIZE", choice_size, .) %>%  # Change Choices Font size
+  str_replace_all(string = ., 
+                  pattern = "replaceID", 
+                  replacement = c(paste0(short_name, "_ins"), paste0(short_name, "_", sprintf("%02d", seq(length(.)-1))))) %>% 
+  cat(qualtrics_codes$advanced_format, 
+      gsub("block_name", long_name, qualtrics_codes$block_start), ., 
+      sep = "\n", 
+      file = file.path(output_dir, "big_five.txt"))

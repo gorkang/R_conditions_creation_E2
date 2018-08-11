8e77d03ad4e1253ea94fa787d55e008822454a33
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-11 16:12:00 -0400

    call script to create gdsm

--- scripts/scales_preparing.R
+++ scripts/scales_preparing.R
@@ -22,11 +22,15 @@ source("scripts/scale_big_five.R")
 # Big five
 source("scripts/scale_crt_7.R")
 
+# General decision-making style
+source("scripts/scale_gdms.R")
+
 # PRINT SCALES (BOOK)
 # this vector with paths set the scales to be printed
 scales2print <- c("materials/qualtrics/output/plain_text/scales/apriori_belief", 
                   "materials/qualtrics/output/plain_text/scales/severity_emotion/partial",
                   "materials/qualtrics/output/plain_text/scales/numeracy/",
-                  "materials/qualtrics/output/plain_text/scales/crt_7/") 
+                  "materials/qualtrics/output/plain_text/scales/crt_7/",
+                  "materials/qualtrics/output/plain_text/scales/gdms/") 
 
 source("scripts/print_scales.R")

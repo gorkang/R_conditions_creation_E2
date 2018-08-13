2d9b7aa91d1916f697aa1e821a4a91cffa3e7e8f
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-13 12:34:47 -0300

    create sns with qualtrics format

---
+++ scripts/scale_sns.R
@@ -0,0 +1,31 @@
+# Packages
+if (!require('pacman')) install.packages('pacman'); library('pacman')
+p_load(tidyverse, magrittr)
+
+# names for item ID
+long_name <- "subjective_numeracy_scale"
+short_name <- "sns"
+
+# Template reading and handling
+subjective_numeracy_scale <-
+  "materials/Scales/input/subjective_numeracy_scale.txt" %>% 
+  readChar(., file.size(.)) %>% # Read text file
+  str_split(., "\n__QSEP__\n") %>% # Separate questions
+  unlist() %>% as.list() %>% # Flatten questions list
+  str_replace_all(string = ., 
+                  pattern = "replaceID", 
+                  replacement = c(paste0(short_name, "_ins"), paste0(short_name, "_", sprintf("%02d", seq(length(.)-1))))) %>% # Add question IDs
+  gsub("Q_FONT_SIZE", question_size, .) %>% # Change Questions Font size
+  gsub("C_FONT_SIZE", choice_size, .) # Change Choices Font size
+
+# Output dir
+output_dir <- 
+  "materials/qualtrics/output/plain_text/scales/subjective_numeracy" %T>% 
+  dir.create(., FALSE, TRUE)
+
+# Export
+subjective_numeracy_scale %>% 
+cat(qualtrics_codes$advanced_format, 
+    gsub("block_name", long_name, qualtrics_codes$block_start),
+    ., 
+    sep = "\n", file = file.path(output_dir, paste0(long_name ,".txt")))

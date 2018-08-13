b6bb7fdcad981397b41e486f424dcda06ff43980
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-08-13 12:35:18 -0300

    call sns script to create qualtrics format and displaying

--- scripts/scales_preparing.R
+++ scripts/scales_preparing.R
@@ -25,12 +25,17 @@ source("scripts/scale_crt_7.R")
 # General decision-making style
 source("scripts/scale_gdms.R")
 
+# General decision-making style ##################
+source("scripts/scale_sns.R")
+
+
 # PRINT SCALES (BOOK)
 # this vector with paths set the scales to be printed
 scales2print <- c("materials/qualtrics/output/plain_text/scales/apriori_belief", 
                   "materials/qualtrics/output/plain_text/scales/severity_emotion/partial",
                   "materials/qualtrics/output/plain_text/scales/numeracy/",
                   "materials/qualtrics/output/plain_text/scales/crt_7/",
-                  "materials/qualtrics/output/plain_text/scales/gdms/") 
+                  "materials/qualtrics/output/plain_text/scales/gdms/",
+                  "materials/qualtrics/output/plain_text/scales/subjective_numeracy//") 
 
 source("scripts/print_scales.R")

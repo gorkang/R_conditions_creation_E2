fb2759be87121e575a78cd19598da5742763e78a
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-09-27 17:17:02 -0300

    fix scales printing: book linebreaks and qualtrics tags removal

--- scripts/print_scales.R
+++ scripts/print_scales.R
@@ -1,4 +1,5 @@
 # READ SCALE
+
 scales <- 
   scales2print %>% 
   map(~dir(.x, ".txt", full.names = TRUE)) %>% unlist() %>% map(~readChar(.x, file.size(.x))) %>% 
@@ -8,14 +9,16 @@ scales <-
 # Function to remove rubish from scales (placeholders, qualtrics tagas, etc.)
 clean_scale <- function(str) {
   str %>% 
-    gsub("\\[{2}Block\\:([a-z_0-9]*)\\]{2}", "**Scale: \\1**", .) %>% # scale name
     gsub("\\[{2}AdvancedFormat\\]{2}\\n", "", .) %>%                  # remove advanceformat tag
-    gsub("\\[{2}ID\\:([a-zA-Z_0-9]*)\\]{2}", "**\\1**", .) %>%        # question ID
-    gsub('<span style="font-size\\:[0-9]{2}px;">', "\\1 ", .) %>%     # remove html tag
-    gsub('</span>', "", .) %>%  # remove html tag
+    gsub("\\[{2}Block\\:([a-z_0-9]*)\\]{2}", "## \\1", .) %>% # scale name
     gsub("\\[{2}Question.*?\\]{2}\\n", "", .) %>% # remove Question qualtrics format 
-    gsub("\\[{2}Choices\\]{2}\\n", "", .) %>%  # remove choices qualtrics format 
-    gsub("\\n", "\n\n", .) %>% # double linebreaks because bookdown is weird
+    gsub("\\[{2}ID\\:([a-zA-Z_0-9]*)\\]{2}", "  \n**\\1**", .) %>%        # question ID
+    gsub("\\[{2}Choices\\]{2}\\n", "  \n", .) %>%  # remove choices qualtrics format 
+    gsub("\\[{2}PageBreak\\]{2}\\n", "  \n", .) %>%  # remove page break qualtrics format 
+    gsub("<span style='font-size\\:[0-9]{2}px;'>", "", .) %>%     # remove html tag
+    gsub("</span>", "", .) %>%  # remove html tag
+    gsub("<br>", " ", .) %>%  # remove html linebreak
+    gsub("\\n", "  \n", .) %>% # double linebreaks because bookdown is weird
     gsub("DELETE_THIS", "", .) # remove DELETE_THIS
 }
 
